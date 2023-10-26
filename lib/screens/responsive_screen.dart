b1c369aa66bcf328a4eb3148008dfa85dc7093a8
mw-sanji <jainshreyans77@gmail.com>
2023-10-26 20:24:44 +0530

    commit

--- lib/screens/responsive_screen.dart
+++ lib/screens/responsive_screen.dart
@@ -16,61 +16,65 @@ class ResponsiveScreen extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: Column(
                 children: [
-                  Container(
-                    height: 200,
-                    color: Colour().Color1,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 400,
-                            color: Colour().Color2,
-                          ),
-                          SizedBox(
-                            height: 50,
-                          ),
-                          Container(
-                            height: 50,
-                            color: Colour().Color3,
-                          )
-                        ],
+                  Card(
+                    child: Container(
+                      height: 200,
+                      color: Colour().Color1,
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              height: 50,
+                              width: 400,
+                              color: Colour().Color2,
+                            ),
+                            SizedBox(
+                              height: 50,
+                            ),
+                            Container(
+                              height: 50,
+                              color: Colour().Color3,
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
                   const SizedBox(
                     height: 50,
                   ),
-                  Container(
-                    height: 200,
-                    child: Stack(
-                      alignment: Alignment.bottomCenter,
-                      children: [
-                        Container(
-                            height: 150,
-                            color: Colour().Color1,
-                            child: Padding(
-                              padding: const EdgeInsets.all(20),
-                              child: Align(
-                                alignment: Alignment.bottomCenter,
-                                child: Container(
-                                  height: 50,
-                                  color: Colour().Color3,
+                  Card(
+                    child: Container(
+                      height: 200,
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          Container(
+                              height: 150,
+                              color: Colour().Color1,
+                              child: Padding(
+                                padding: const EdgeInsets.all(20),
+                                child: Align(
+                                  alignment: Alignment.bottomCenter,
+                                  child: Container(
+                                    height: 50,
+                                    color: Colour().Color3,
+                                  ),
                                 ),
-                              ),
-                            )),
-                        Positioned(
-                          top: 20,
-                          child: Container(
-                            height: 50,
-                            width: 200,
-                            color: Colour().Color2,
-                          ),
-                        )
-                      ],
+                              )),
+                          Positioned(
+                            top: 20,
+                            child: Container(
+                              height: 50,
+                              width: 200,
+                              color: Colour().Color2,
+                            ),
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 ],
